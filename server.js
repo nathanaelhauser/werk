c2ae18a35e86b55548b42856fa0e92b751720a26
Mercedes Walsh <sadieswalsh@gmail.com>
2020-01-16 19:59:32 -0800

    initial files and folders

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const { join } = require('path')
+
+const app = express()
+
+app.use(express.static(join(__dirname, 'client', 'build')))
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+
+app.listen(process.env.PORT || 3001)
