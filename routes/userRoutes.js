e572752038706bf166e582381a06e0a6b2533723
yokevi <you.k2208@gmail.com>
2020-01-16 21:24:15 -0800

    updated routes - index and userRoutes

---
+++ routes/userRoutes.js
@@ -0,0 +1,18 @@
+const { Users } = require('../Models/Users.js')
+
+module.exports = app => {
+
+  // GET ONE USER INFO
+  app.get('/users/:id', (req, res) => {
+    Users.find({ _id: req.params.id })
+      .then(user => res.JSON(user))
+      .catch(e => console.log(e))
+  })
+
+  //POST USER INFO
+  app.post('/users', (req, res) => {
+    Users.create(req.body)
+      .then(() => res.sendStatus(200))
+      .catch(e => console.log(e))
+
+}
