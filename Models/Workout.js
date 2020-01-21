27245fdceb3d18d28afd172f859645e506dcce31
yokevi <you.k2208@gmail.com>
2020-01-20 16:36:42 -0800

    models/routes-workout exercise relationship

--- Models/Workout.js
+++ Models/Workout.js
@@ -2,7 +2,7 @@ module.exports = (model, Schema) => {
 
   const Workout = new Schema({
     name: String,
-    excercises: [String]
+    exercises: [{ type: Schema.Types.ObjectId, ref: 'Exercise' }]
   })
 
   return model('Workout', Workout)
--- Models/index.js
+++ Models/index.js
@@ -2,6 +2,6 @@ const {model, Schema} = require('mongoose')
 
 const User = require('./User.js')(model, Schema)
 const Workout = require('./Workout')(model, Schema)
-// const Exercise = require('./Exercise.js')(model, Schema)
+const Exercise = require('./Exercise.js')(model, Schema)
 
-module.exports = { User, Workout }
+module.exports = { User, Workout, Exercise }
