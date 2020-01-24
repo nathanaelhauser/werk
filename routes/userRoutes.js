147b457867384d55c1b6fa3c85c1d20fb66bdd38
chbbs231 <chbbs231@yahoo.com>
2020-01-23 19:10:02 -0800

    working on passport

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,24 @@
-const { User } = require('../Models')
+const { Users } = require('../Models')
 
 module.exports = app => {
 
   // GET ALL USERS 
   app.get('/users', (req, res) => {
-    User.find()
+    Users.find()
       .then(users => res.JSON(users))
       .catch(e => console.error(e))
   })
 
   // GET ONE USER 
   app.get('/users/:id', (req, res) => {
-    User.find({ _id: req.params.id })
+    Users.find({ _id: req.params.id })
       .then(user => res.JSON(user))
       .catch(e => console.error(e))
   })
 
   //POST USER 
   app.post('/users', (req, res) => {
-    User.create(req.body)
+    Users.create(req.body)
       .then(() => res.sendStatus(200))
       .catch(e => console.error(e))
   })
--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 require('./config')
 const express = require('express')
 const { join } = require('path')
+const passport = require('passport')
+const {Strategy} = require('passport-local')
+const {Strategy: JWTStrategy, ExtractJwt} =('passport-jwt')
 
 const app = express()
 
 app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new Strategy(User.Authenticate()))
+passport.serializeUser(User.serializerUser())
+passport.deserializeUser(User.deserializerUser())
+
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    
+}, ({id}, cb) => User.findById(id)
+.then(user => cb(null, user))
+.catch(e => cb(e))))
 
 require('./routes')(app)
 
