78778f1ebfb38eeef44278ad67a2bc3bb976525f
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-01-23 18:58:40 -0800

    Merging with master

---
+++ Models/Exercise.js
@@ -0,0 +1,14 @@
+module.exports = (model, Schema) => {
+
+  const Exercise = new Schema({
+    name: String,
+    category: String,
+    equipment: [String],
+    description: String,
+    mainMuscles: [String],
+    secondaryMuscles: [String]
+  })
+
+  return model('Exercise', Exercise)
+  
+}
