651552364ef11b88c854d279b55c62893dcf9084
chbbs231 <chbbs231@yahoo.com>
2020-01-25 11:21:06 -0800

    reg almost done

--- client/src/components/RegForm/RegForm.js
+++ client/src/components/RegForm/RegForm.js
@@ -1,145 +1,64 @@
 import React, {useContext} from 'react';
-import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import FormControl from '@material-ui/core/FormControl'
+import Input from '@material-ui/core/Input'
+import InputAdornment from '@material-ui/core/InputAdornment'
 
-const useStyles = makeStyles({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: 200,
-  },
-});
+const useStyles = makeStyles(theme => ({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: 200,
+      },
+    },
+  }));
 
 const RegForm = () =>{
     const classes = useStyles();
 
     return (
+    <Container  maxWidth ="sm">
       <div className={classes.root}>
         <div>
-          <TextField
-            id="standard-full-width"
-            label="Label"
-            style={{ margin: 8 }}
-            placeholder="Placeholder"
-            helperText="Full width!"
-            fullWidth
-            margin="normal"
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-          <TextField
-            label="None"
+        <TextField
+            label="Username"
             id="margin-none"
-            defaultValue="Default Value"
             className={classes.textField}
-            helperText="Some important text"
           />
           <TextField
-            label="Dense"
-            id="margin-dense"
-            defaultValue="Default Value"
+            label="Name"
+            id="margin-none"
             className={classes.textField}
-            helperText="Some important text"
-            margin="dense"
           />
           <TextField
-            label="Normal"
-            id="margin-normal"
-            defaultValue="Default Value"
+            label="Password"
+            id="margin-none"
             className={classes.textField}
-            helperText="Some important text"
-            margin="normal"
           />
-        </div>
-        <div>
           <TextField
-            id="filled-full-width"
-            label="Label"
-            style={{ margin: 8 }}
-            placeholder="Placeholder"
-            helperText="Full width!"
-            fullWidth
-            margin="normal"
-            InputLabelProps={{
-              shrink: true,
+            label="Comfirm Password"
+            id="margin-none"
+            className={classes.textField}
+          />
+          <FormControl/>
+          <Input
+            id="standard-adornment-weight"
+           // value={values.weight}
+            //onChange={handleChange('weight')}
+            endAdornment={<InputAdornment position="end">Lb</InputAdornment>}
+            aria-describedby="standard-weight-helper-text"
+            inputProps={{
+              'aria-label': 'weight',
             }}
-            variant="filled"
-          />
-          <TextField
-            label="None"
-            id="filled-margin-none"
-            defaultValue="Default Value"
-            className={classes.textField}
-            helperText="Some important text"
-            variant="filled"
-          />
-          <TextField
-            label="Dense"
-            id="filled-margin-dense"
-            defaultValue="Default Value"
-            className={classes.textField}
-            helperText="Some important text"
-            margin="dense"
-            variant="filled"
-          />
-          <TextField
-            label="Normal"
-            id="filled-margin-normal"
-            defaultValue="Default Value"
-            className={classes.textField}
-            helperText="Some important text"
-            margin="normal"
-            variant="filled"
           />
         </div>
-        <div>
-          <TextField
-            id="outlined-full-width"
-            label="Label"
-            style={{ margin: 8 }}
-            placeholder="Placeholder"
-            helperText="Full width!"
-            fullWidth
-            margin="normal"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="outlined"
-          />
-          <TextField
-            label="None"
-            id="outlined-margin-none"
-            defaultValue="Default Value"
-            className={classes.textField}
-            helperText="Some important text"
-            variant="outlined"
-          />
-          <TextField
-            label="Dense"
-            id="outlined-margin-dense"
-            defaultValue="Default Value"
-            className={classes.textField}
-            helperText="Some important text"
-            margin="dense"
-            variant="outlined"
-          />
-          <TextField
-            label="Normal"
-            id="outlined-margin-normal"
-            defaultValue="Default Value"
-            className={classes.textField}
-            helperText="Some important text"
-            margin="normal"
-            variant="outlined"
-          />
-        </div>
+       <br/>
+        <Button variant="contained" color="primary"> Submit </Button>
       </div>
+      </Container>
     );
   }
 export default RegForm
