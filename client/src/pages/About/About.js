7f2b9b59089259965102449103778ebb69addaa9
yokevi <you.k2208@gmail.com>
2020-01-25 10:34:21 -0800

    added DevCard component for About

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Nav from '../../components/Nav'
+import DevCard from '../../components/DevCard'
 
 const About = () => {
     return (
-        <>
-        </>
+        <div>
+            <Nav />
+            <DevCard />
+        </div>
     )
 }
 
