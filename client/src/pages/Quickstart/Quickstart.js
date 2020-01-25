59269c5940c613b15fc1c595974b0c474d92487f
Maverick <maverick.schirmer@gmail.com>
2020-01-25 11:31:02 -0800

    adjusted color to nav bar

--- client/src/pages/Quickstart/Quickstart.js
+++ client/src/pages/Quickstart/Quickstart.js
@@ -30,11 +30,11 @@ const Quickstart = () => {
   return (
     <Grid container spacing={4} direction='row' justify="space-around" alignItems="center">
      <Grid item > <h1> What area do you want to werk on?</h1></Grid>
-      <Grid item xs={12} sm={6} md={4} className={classes.buttonContainer}>
+      <Grid item xs={12} sm={6} md={6} className={classes.buttonContainer}>
         <UBModal/>
       </Grid>
    =
-      <Grid item xs={12} sm={6} md={4} className={classes.buttonContainer}>
+      <Grid item xs={12} sm={6} md={6} className={classes.buttonContainer}>
         <LBModal/>
       </Grid>
   
