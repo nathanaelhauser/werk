1dda669d0d2b37710ea458c25e68e317066c6133
chbbs231 <chbbs231@yahoo.com>
2020-01-26 13:06:21 -0800

    will work on it

--- client/src/pages/Custom/Custom.js
+++ client/src/pages/Custom/Custom.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import MyProfileCard from '../../components/MyProfileCard'
 const MyProfile = () => {
   return(
     <div>
-      
+      <MyProfileCard/>
     </div>
   )
 }
