34fdf019ab4e184314b6ac286f04afa85f2352a8
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-01-30 18:34:22 -0800

    Added proxy route and modified customForm

--- client/src/components/CustomForm/CustomForm.js
+++ client/src/components/CustomForm/CustomForm.js
@@ -1,24 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import Container from '@material-ui/core/Container'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
 
 const CustomForm = () => {
+
+  useEffect(() => {
+  }, [])
+
   return (
     <Container>
       <Paper>
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          alignItems="center"
-          spacing={3}
-        >
-          <Grid item xs={12} md={6}>
-            
+        <form noValidate>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing={3}
+          >
+            <Grid item xs={12} md={6}>
+              <TextField id="workout-input" label="Your Workout" variant="outlined" />
+            </Grid>
           </Grid>
-        </Grid>
+        </form>
       </Paper>
     </Container>
   )
 }
+
+export default CustomForm
