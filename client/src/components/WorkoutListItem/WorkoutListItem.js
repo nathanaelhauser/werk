0bc8a7ea7dcc95cf94e4215a1d9843f42600ffdb
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-02-07 04:42:41 -0800

    Quickstart works; modified WorkoutListItem

--- client/src/components/WorkoutListItem/WorkoutListItem.js
+++ client/src/components/WorkoutListItem/WorkoutListItem.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { Redirect } from 'react-router-dom'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import IconButton from '@material-ui/core/IconButton'
 import LibraryAddIcon from '@material-ui/icons/LibraryAdd'
+import QuickStartContext from '../../utils/QuickstartContext'
+import WorkoutContext from '../../utils/WorkoutContext'
 
 const WorkoutListItem = props => {
+  const { setWorkout } = useContext(WorkoutContext)
+  const [goWorkout, setGoWorkout] = useState(false)
+
+  const choseWorkout = event => {
+    setWorkout(props.workout)
+    setGoWorkout(true)
+  }
+
+  const renderRedirectWorkout = () => {
+    if (goWorkout) {
+      return <Redirect to="/workout" />
+    }
+  }
+
   return (
     <ListItem>
+      {renderRedirectWorkout()}
       <ListItemText
         primary={props.workout.name}
       />
       <ListItemSecondaryAction>
-        <IconButton edge="end" aria-label="delete">
+        <IconButton edge="end" aria-label="delete" onClick={choseWorkout}>
           <LibraryAddIcon />
         </IconButton>
       </ListItemSecondaryAction>
