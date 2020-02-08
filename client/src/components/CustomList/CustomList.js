b19a7c86d6b2195bcbf7337b8d86aecff4db46c6
Maverick <maverick.schirmer@gmail.com>
2020-02-08 11:22:32 -0800

    Custom Form Changes

--- client/src/components/CustomList/CustomList.js
+++ client/src/components/CustomList/CustomList.js
@@ -4,10 +4,17 @@ import { makeStyles } from '@material-ui/core/styles'
 import IconButton from '@material-ui/core/IconButton'
 import RemoveIcon from '@material-ui/icons/Delete'
 import Button from '@material-ui/core/Button'
-import Switch from '@material-ui/core/Switch';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Grid from '@material-ui/core/Grid'
 
+const useGridStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  
+}));
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -25,15 +32,16 @@ titleTypography: {"fontFamily": "\"Bangers\"",
     }))
 
 const CustomList = () => {
-  const [checked, setChecked] = React.useState(false);
-
+  const [checked, setChecked] = React.useState(false)
+  const gridClasses = useGridStyles()
   const toggleChecked = () => {
-    setChecked(prev => !prev);
-  };
+    setChecked(prev => !prev)
+  }
   const classes = useStyles()
   const { exercises, handleCustomRemoveExercise, workoutTitle, handleCustomAddWorkout } = useContext(CustomContext)
 
   return(
+  
     <div className={classes.root}>
       {
         <p className={classes.titleTypography} value={workoutTitle}>{workoutTitle}</p>
@@ -42,7 +50,7 @@ const CustomList = () => {
       exercises.length ? exercises.map(exercise => (
         <p className={classes.typography}>
           <span>{exercise.name}</span>
-          <IconButton aria-label="delete" onClick={() => handleCustomRemoveExercise(exercise._id)}><RemoveIcon /></IconButton>
+          <IconButton size="small" aria-label="delete" onClick={() => handleCustomRemoveExercise(exercise._id)}><RemoveIcon fontSize="small"/></IconButton>
         </p>
         )) 
          : null
