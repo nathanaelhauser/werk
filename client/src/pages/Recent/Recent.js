b517dd4cc7c07c723196aa78790ace0f223661ae
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-02-11 03:28:11 -0800

    Added WorkoutPanel and modified Event model

--- client/src/pages/Recent/Recent.js
+++ client/src/pages/Recent/Recent.js
@@ -1,21 +1,41 @@
 import React, { useEffect, useState } from 'react'
 import EventAPI from '../../utils/EventAPI'
+import Container from '@material-ui/core/Container'
+import WorkoutPanel from '../../components/WorkoutPanel'
 
 const Recent = () => {
   const [ recentState, setRecentState ] = useState({
-    workouts: []
+    recentWorkouts: []
   })
+  const [ expanded, setExpanded ] = useState('panel1')
+
+  const handleChange = panel => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false)
+  }
 
   useEffect(() => {
     EventAPI.getUserEvents()
-      .then(({ data }) => {
-        console.log(data)
+      .then(({ data: recentWorkouts }) => {
+        setRecentState({ ...recentState, recentWorkouts })
+        console.log(recentWorkouts)
       })
       .catch(e => console.error(e))
   }, [])
 
   return (
-    <h1>Recent Workouts</h1>
+    <Container>
+      <div>
+        <WorkoutPanel 
+          expanded={expanded} 
+          handleChange={handleChange} 
+          name='Workout Name'
+          date='Jan 17, 2020 7:35pm'
+          id="5e3d4dcd673c713b0c043ecd"
+          area='upper'
+          exercises={[{name:'Bob'},{name:'John'},{name:'Jimmy'}]}
+        />
+      </div>
+    </Container>
   )
 }
 
