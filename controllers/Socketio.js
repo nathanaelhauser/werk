52b59db2882b08df674d00a747d33c7eb4e03bc7
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-02-11 21:43:56 -0800

    Added socket.io test code

--- controllers/Socketio.js
+++ controllers/Socketio.js
@@ -1,14 +1,11 @@
-const { Server } = require('http')
-
-module.exports = (app) => {
-
-  const server = Server(app)
+module.exports = app => {
+  const server = require('http').Server(app)
   const io = require('socket.io')(server)
-
   server.listen(80)
 
   io.on('connection', socket => {
-    
+    socket.on('id', data => {
+      console.log(data)
+    })
   })
-
 }
--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,3 @@
-module.exports = (app) => {
-
+module.exports = app => {
   require('./Socketio')(app)
-
 }
